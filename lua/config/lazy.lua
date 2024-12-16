8e16438c9e426a2eb024a4eea9463a639bb530d5
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2024-12-15 16:06:29 -0800

    Changing startup items

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,18 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
-    { import = "lazyvim.plugins.extras.vscode" },
-
-
     -- import/override with your plugins
     { import = "plugins" },
   },
